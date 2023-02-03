021ffc573dc21a68a27725d459f8e913a04861ca
Alena Dubadzelava <dubodelova.minsk@gmail.com>
2023-02-03 12:27:54 +0100

    done

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first > second) {
+            System.out.printf("%d\n",first);
+        } else if (first < second){
+            System.out.printf("%d\n", second);
+        } else {
+        }
     }
 }
